1f1f16b384853780d75946ab89a8284cc928a08b
R. Alex Barbieri <luck@mit.edu>
2013-08-05 11:05:26 -0400

    cleanup of 44x branch

--- CmsHi/PhotonAnalysis/script/checkOut.sh
+++
@@ -1,4 +0,0 @@
-cvs co -d UserCode/HafHistogram UserCode/CmsHi/HafHistogram
-cvs co -d CmsHi/PhotonAnalysis UserCode/CmsHi/PhotonAnalysis
-cvs co UserCode/MitHig/PixelTrackletAnalyzer
-mv UserCode/MitHig .
--- CmsHi/PhotonAnalysis/script/setupRandomConeAna.sh
+++
@@ -1,5 +0,0 @@
-cd $CMSSW_BASE/src
-
-cvs co -d RandomConeAna UserCode/EYKim/photonStudy/RandomConeAna
-
-# don't need the Supercluster seed patch anymore. 
